93cdab6381c55783ac31f85de05919fcca1d16a0
swopno1 <amirhossain.limon@gmail.com>
2025-11-01 02:45:13 +0600

    Added all routes

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>LandingPage</h1>
+      <p>This page will dispaly sign in or sign up page</p>
+    </div>
+  );
+};
+
+export default LandingPage;
